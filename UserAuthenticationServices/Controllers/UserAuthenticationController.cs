9eb90113dad38111e8b9d2367d1a0d9ef4b43f38
Patryk Nowak <Patrykjannowak@gmail.com>
2023-01-13 20:15:19 +0100

    Created UserAuthenticationController

---
+++ UserAuthenticationServices/Controllers/UserAuthenticationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserAuthenticationServices.Controllers;
+
+[ApiController]
+[Route("userAuthenticationController/1.0/")]
+public class UserAuthenticationController : ControllerBase
+{
+    [HttpGet("Get")]
+    public async Task<string> GetUserData()
+    {
+        return "SomeDefaultData";
+    }
+}
